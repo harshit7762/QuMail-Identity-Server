4f62c3fe32ab30131369356b03dc189418068461
Harshit Mishra <harshitmishra7762@gmail.com>
2026-03-08 22:42:26 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
-from flask import Flask
-# ... your logic ...
-app = Flask(__name__)
-# ... your routes ...
+from identity_server import app  # This pulls the logic from your main file
 
 if __name__ == "__main__":
     app.run()
